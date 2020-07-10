a0d934ceb6c47ad58951df0954a2de0667072e6a
AR1988 <37342319+AR1988@users.noreply.github.com>
2020-07-10 19:14:33 +0200

    init

---
+++ 10_07_20_task2/src/main/java/gameUnitTests/Game.java
@@ -0,0 +1,35 @@
+package gameUnitTests;
+
+public class Game {
+    private Word word;
+    private int balance;
+    private int attempts;
+    private int delta = 5;
+    private int winKoef = 10;
+    private int looseKoef = 5;
+    public int getBalance() {
+        return balance;
+    }
+    public int getAttempts() {
+        return attempts;
+    }
+    public void startGame(String w, String d) {
+        word = new Word(w, d);
+    }
+    public void openLetter(char letter) {
+        int match = word.openLetter(letter);
+        if (match == 0) balance -= delta;
+        else
+            balance += match * delta;
+        attempts++;
+    }
+    public boolean isWin() {
+        return word.isWin();
+    }
+    public void openWord(String w) throws WrongWordLengthException {
+        attempts = attempts+1;
+        if (word.openWord(w) == true) balance += delta * winKoef;
+        else balance -= delta * looseKoef;
+        attempts++;
+    }
+}
